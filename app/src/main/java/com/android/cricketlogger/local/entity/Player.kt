d876758f7f53f1dde6949be04ca36571a5dcf6ce
myofficework000 <cheetahmail007@gmail.com>
2024-05-11 01:49:05 +0530

    Updated Dao

--- app/src/main/java/com/android/cricketlogger/local/entity/Player.kt
+++ app/src/main/java/com/android/cricketlogger/local/entity/Player.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity("NewPlayerTable")
-data class NewPlayer(
+data class Player(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val name: String,
